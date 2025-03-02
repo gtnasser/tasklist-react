5be1dff6c997b7c677c7330896274cc7c63166c5
Giba Nasser <gtnasser@gmail.com>
2025-03-02 13:38:11 -0300

    feat: comment intial load from external api

--- src/App.jsx
+++ src/App.jsx
@@ -33,15 +33,15 @@ function App() {
         localStorage.setItem("tasks", JSON.stringify(tasks))
     }, [tasks])
 
-    // carregar dados de uma API
-    useEffect(() => {
-        async function fetchTasks() {
-            const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
-            const data = await response.json()
-            setTasks(data)
-        }
-        fetchTasks()
-    }, [])
+    // carregar dados de uma API no início da aplicação
+    // useEffect(() => {
+    //     async function fetchTasks() {
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    //         const data = await response.json()
+    //         setTasks(data)
+    //     }
+    //     fetchTasks()
+    // }, [])
 
     function onTaskClick(taskId) {
         const newTasks = tasks.map((task) => {
